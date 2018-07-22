fbdea1b12acfc970ce2aebe2b232eb6bd8960d49
josh.ray.carr@gmail.com <josh.ray.carr@gmail.com>
2018-07-22 18:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday,name) {
   return ('Happy ${holiday}, ${name}!')
 }
-function holidayCountdown(days,holiday) {
+function holidayCountdown(20, Mother\'s day) {
   return ("It's ${days} days until ${holiday}!")
 }
